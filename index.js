871ea5679003d98ecb3604a3af2ae135d5f4a00f
NULLYUKI <no@no.no>
2023-06-24 03:07:36 +0200

    Remove comment syntax to be able to use the moveable window function.

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import './assets/endmysuffering.css';
 import * as utils from './utils.js';
 import * as Tweakpane from './tweakpane.js';
 
+
 window.addEventListener('load', async () => {
    console.log('Hello, world!');
-
+   
    window.Effect.apply('unified', { color: '#0009' });
-
+   
    const div = document.createElement('div');
    let element = document.createElement('div');
    div.classList.add('tweakpane-container-wrapper');
@@ -17,12 +18,12 @@ window.addEventListener('load', async () => {
    div.appendChild(element);
    document.body.appendChild(div);
    utils.makeDraggable(element);
-
+   
    // TODO: Itll sometimes show undefined
    // Find out why
-
+   
    // Extra: Should now work? Need to investigate
-
+   
    while (!(await utils.getUsername())) {
       await new Promise((resolve) => setTimeout(resolve, 1000));
    }
@@ -35,10 +36,9 @@ window.addEventListener('load', async () => {
 
 // also a bug is when you drag it out ill not react. just p[ress a boolean
 // and it will react again
-
-/* let element = document.getElementsByClassName('tweakpane-container')[0];
+let element = document.getElementsByClassName('tweakpane-container')[0];
 
 element.ondragend = function (e) {
    utils.checkOutOfBounds('tweakpane-container');
 };
- */
+
