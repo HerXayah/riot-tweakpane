ea56823cd596f7c88e9b571def2996ac27938886
Sarah Engel <45071533+PrincessAkira@users.noreply.github.com>
2024-05-28 16:16:31 +0200

    aaaaaaaaaaaa

--- linkhandler.js
+++ linkhandler.js
@@ -5,12 +5,12 @@ export async function processLink(link) {
       if (url.hostname === 'www.youtube.com' && url.pathname === '/watch') {
          const videoId = url.searchParams.get('v');
          if (videoId) {
-            return `https://www.youtube.com/embed/${videoId}`;
+            return `https://cdpn.io/pen/debug/oNPzxKo?v=${videoId}`;
          }
       } else if (url.hostname === 'youtu.be') {
          const videoId = url.pathname.slice(1); // youtu.be/VIDEO_ID
          if (videoId) {
-            return `https://www.youtube.com/embed/${videoId}`;
+            return `https://cdpn.io/pen/debug/oNPzxKo?v=${videoId}`;
          }
       } else {
          throw new Error('Invalid YouTube link');
@@ -21,9 +21,7 @@ export async function processLink(link) {
 }
 
 export async function replaceLinks(link) {
-   if (link.includes('youtube.com')) {
-      link = `${link}?showinfo=0&rel=0&autohide=1&autoplay=1&widget_referrer=leagueclient`;
-   }
+   console.log(link);
 
    const element = document.querySelector(
       '#rcp-fe-viewport-root > section.rcp-fe-viewport-overlay > div:nth-child(3) > lol-uikit-full-page-modal > span > div > iframe'
@@ -33,9 +31,11 @@ export async function replaceLinks(link) {
       throw new Error('Iframe element not found');
    }
 
-   if (link.includes('youtube.com')) {
+   if (link.includes('cdpn.io')) {
       element.src = link;
    } else {
       throw new Error('Invalid YouTube link');
    }
+
+   element.src = link;
 }
